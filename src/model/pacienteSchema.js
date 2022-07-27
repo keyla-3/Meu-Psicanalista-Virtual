30fdc834dab97e391ed86fbd31d37c4a2ed57faf
keyla-3 <keylafcp@gmail.com>
2022-07-27 16:01:37 -0300

    criando o schema

---
+++ src/model/pacienteSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const pacientesSchema = mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    nome: {
+        type: String,
+        required: true
+    },
+    contato: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    dataNascimento: {
+        type: String,
+        required: true
+    },
+    endereco: {
+        type: String,
+        required: true
+    },
+    trabalhoStatus: {
+        type: String,
+        enum: ['trabalhando', 'desempregado', 'freelancer'],
+        required: true,
+        default: 'trabalhando'
+    },
+    horarioAtendimento: {
+        type: String,
+        enum: ['manha', 'tarde', 'noite'],
+        default: 'manha',
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('paciente', pacientesSchema);
